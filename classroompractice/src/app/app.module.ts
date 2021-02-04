1ed79bd9e05046b1874a46789577235c849f0625
Iskander Dosgali <idosgali@gmail.com>
2021-02-04 14:21:19 +0600

    Изменил app.module.ts добавил в нутерь my2 компонент

--- classroompractice/src/app/app.module.ts
+++ classroompractice/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {MyComponent} from './my/my.component';
+import {My2Component} from './my2/my2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyComponent
+    MyComponent,
+    My2Component,
   ],
   imports: [
     BrowserModule,

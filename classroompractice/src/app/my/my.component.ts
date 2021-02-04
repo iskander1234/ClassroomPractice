d2cb7d8e2b22ddfc610fc0784ac0c126158a06ce
Iskander Dosgali <idosgali@gmail.com>
2021-02-04 14:16:59 +0600

    Изменил my.component

--- classroompractice/src/app/my/my.component.ts
+++ classroompractice/src/app/my/my.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my',
   templateUrl: './my.component.html',
+  // styleUrls: ['./my.component.styl'],
 })
 export class MyComponent{
   title = 'classroompractice';

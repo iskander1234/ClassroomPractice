156745c82763ae98c221cd91368a816f67aa7fea
Iskander Dosgali <idosgali@gmail.com>
2021-02-04 13:41:06 +0600

    Измнгил чтоб my.component работал в app.component.html and app.module.ts

--- classroompractice/src/app/app.module.ts
+++ classroompractice/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MyComponent} from './my/my.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyComponent
   ],
   imports: [
     BrowserModule,

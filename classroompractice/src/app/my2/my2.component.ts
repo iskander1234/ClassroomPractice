3c79aed31e2859043b0ba5fb4bdc3d4bc23eadca
Iskander Dosgali <idosgali@gmail.com>
2021-02-04 14:21:54 +0600

    Изменил my2.component.ts добавил класс

---
+++ classroompractice/src/app/my2/my2.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my2',
+  templateUrl: './my2.component.html',
+  // styleUrls: ['./my.component.styl'],
+})
+export class My2Component{
+  title = 'classroompractice';
+}

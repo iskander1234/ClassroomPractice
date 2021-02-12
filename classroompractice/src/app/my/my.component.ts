0a23d086bf52e6d58b95bdcccef3a019d945f834
Iskander Dosgali <idosgali@gmail.com>
2021-02-12 23:15:32 +0600

    Изменил my component это один add.record

--- classroompractice/src/app/my/my.component.ts
+++ classroompractice/src/app/my/my.component.ts
@@ -3,8 +3,34 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-my',
   templateUrl: './my.component.html',
-  // styleUrls: ['./my.component.styl'],
 })
 export class MyComponent{
-  title = 'classroompractice';
+
+  date = '';
+  distance = '';
+  time = '';
+  average = '6.00';
+  options = 'Edit Delete';
+
+  results: [{data: string, distances: string, times: string, avarages: string, optionss: string}] = [{
+    data: 'adsd',
+    distances: 'adsd',
+    times: '132',
+    avarages: 'ads',
+    optionss: 'sfdf'
+  }];
+
+  constructor() {
+  }
+
+  // tslint:disable-next-line:typedef
+  addRecord(){
+    this.results.push({
+      avarages: this.average,
+      distances: this.distance,
+      optionss: this.options,
+      times: this.time,
+      data: this.date
+    });
+  }
 }

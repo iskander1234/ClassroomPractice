fedbbb88cd19767eabcd79246d8522d4bbdcf83c
Iskander Dosgali <idosgali@gmail.com>
2021-02-12 23:16:07 +0600

    Изменил my2 component это один my.results

--- classroompractice/src/app/my2/my2.component.ts
+++ classroompractice/src/app/my2/my2.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component , Input} from '@angular/core';
 
 @Component({
   selector: 'app-my2',
   templateUrl: './my2.component.html',
-  // styleUrls: ['./my.component.styl'],
 })
 export class My2Component{
-  title = 'classroompractice';
+
+  // tslint:disable-next-line:no-input-rename
+   @Input('my2Item') my2 !: { date: string, distance: string, time: string, average: string, options: string, };
+
+   private my2Item = false;
+
 }
